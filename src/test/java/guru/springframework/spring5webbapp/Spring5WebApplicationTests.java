e62cef066b421946b6bc31163d4be864b73a5b31
Sikander Bakht <sikander.bakht@nagarro.com>
2022-12-04 15:26:23 +0300

    initial comit for maven spring project

---
+++ src/test/java/guru/springframework/spring5webbapp/Spring5WebApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
